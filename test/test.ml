6534a1726f2a48b24ea92c4618d8ea0ada7fc06f
Steven Troxler <steven.troxler@gmail.com>
2018-09-08 09:11:25 -0700

    Fix failing test
    I had missed a dtype mismatch that made the printer not work for
    the `list_max` test.
    
    Also, it's pretty clear to me at this point that I'm going to need
    `ppx_deriving` in order to not get annoyed at the difficulty of
    writing tests that print out actual vs expected.

--- test/test.ml
+++ test/test.ml
@@ -3,6 +3,12 @@ open OUnit2
 let int_tcase expected actual =
   (fun _ -> assert_equal expected actual ~printer:string_of_int)
 
+let string_of_int_option = function
+  | None -> "None"
+  | Some x -> "Some " ^ string_of_int x
+
+let int_option_tcase expected actual =
+  (fun _ -> assert_equal expected actual ~printer:string_of_int_option)
 
 let tests = "test suite for sum" >::: [
     "fact_0" >:: int_tcase 1 @@ Math.fact 1;
@@ -10,7 +16,8 @@ let tests = "test suite for sum" >::: [
     "gcd_0"  >:: int_tcase 1 @@ Math.gcd 1 10;
     "gcd_1"  >:: int_tcase 4 @@ Math.gcd 124 56;
   ] @ [
-      "list_max []" >:: int_tcase 5 @@ Math.list_max [1; 5; 4; -3];
+    "list_max empty" >:: int_option_tcase None @@ Math.list_max [];
+    "list_max nonempty" >:: int_option_tcase (Some 5) @@ Math.list_max [1; 5; 4; -3];
   ]
 
 let _ = run_test_tt_main tests
